5403833c4fe9da0d9be14adb3136e1f44507e8ec
Tieg Zaharia <tieg@kickstarter.com>
2012-05-02 11:31:15 -0400

    rails 3.2.* compatibility: fixing select_all method in QueryCacheCompat to properly form cache key

--- lib/slave_pools/query_cache_compat.rb
+++ lib/slave_pools/query_cache_compat.rb
@@ -4,7 +4,9 @@ module SlavePoolsModule
 
     def select_all(*a, &b)
       if @query_cache_enabled
-        cache_sql(a.first, a.last) {send_to_current(:select_all, *a, &b)}
+        arel, name, binds = a
+        sql = to_sql(arel, binds)
+        cache_sql(sql, binds) {send_to_current(:select_all, *a, &b)}
       else
         send_to_current(:select_all, *a, &b)
       end
