160dcdc32296140009b515a0ba6f9093c27da25b
Tieg Zaharia <tieg@kickstarter.com>
2012-05-02 15:17:07 -0400

    rails 3.2.* compatibility: pass prepared sql instead of arel to select_all in QueryCacheCompat

--- lib/slave_pools/query_cache_compat.rb
+++ lib/slave_pools/query_cache_compat.rb
@@ -4,14 +4,17 @@ module SlavePoolsModule
 
     def select_all(*a, &b)
       if @query_cache_enabled
+        # FIXME this will still hit the +select_all+ method in AR's 
+        # query_cache.rb. It'd be nice if we could avoid it somehow so 
+        # +select_all+ and then +to_sql+ aren't called redundantly.
         arel, name, binds = a
         sql = to_sql(arel, binds)
-        cache_sql(sql, binds) {send_to_current(:select_all, *a, &b)}
+        cache_sql(sql, binds) {send_to_current(:select_all, *[sql, name, binds], &b)}
       else
         send_to_current(:select_all, *a, &b)
       end
     end
-    
+
     def columns(*a, &b)
       if @query_cache_enabled
         cache_sql(a.first, a.last) {send_to_current(:columns, *a, &b)}
