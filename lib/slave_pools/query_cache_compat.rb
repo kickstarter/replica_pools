5bdbcdf607e5602b862df4f618232b640bcc1cfa
Tieg Zaharia <tieg@kickstarter.com>
2012-05-03 15:27:10 -0400

    rails 3.2.* compatibility: checking for query_cache_enabled in master as well as current connection (see comments), and removing columns method (this one would get stored as nil in query_cache, thus pointless (the real columns method has a table arg and is in SchemaCache now)

--- lib/slave_pools/query_cache_compat.rb
+++ lib/slave_pools/query_cache_compat.rb
@@ -3,8 +3,8 @@ module SlavePoolsModule
   module QueryCacheCompat
 
     def select_all(*a, &b)
-      if @query_cache_enabled
-        # FIXME this will still hit the +select_all+ method in AR's 
+      if query_cache_enabled
+        # FIXME this still hits the +select_all+ method in AR connection's 
         # query_cache.rb. It'd be nice if we could avoid it somehow so 
         # +select_all+ and then +to_sql+ aren't called redundantly.
         arel, name, binds = a
@@ -15,27 +15,31 @@ module SlavePoolsModule
       end
     end
 
-    def columns(*a, &b)
-      if @query_cache_enabled
-        cache_sql(a.first, a.last) {send_to_current(:columns, *a, &b)}
-      else
-        send_to_current(:columns, *a, &b)
-      end
-    end
-    
     def insert(*a, &b)
-      clear_query_cache if @query_cache_enabled
+      clear_query_cache if query_cache_enabled
       send_to_master(:insert, *a, &b)
     end
     
     def update(*a, &b)
-      clear_query_cache if @query_cache_enabled
+      clear_query_cache if query_cache_enabled
       send_to_master(:update, *a, &b)
     end
     
     def delete(*a, &b)
-      clear_query_cache if @query_cache_enabled
+      clear_query_cache if query_cache_enabled
       send_to_master(:delete, *a, &b)
+    end
+
+    # Rails 3.2 changed query cacheing a little and affected slave_pools like this: 
+    # 
+    #   * ActiveRecord::Base.cache sets @query_cache_enabled for current connection
+    #   * ActiveRecord::QueryCache middleware (in call()) that Rails uses sets 
+    #     @query_cache_enabled directly on ActiveRecord::Base.connection 
+    #     (which could be master at that point)
+    # 
+    # :`( Thusly, we have to check for both.
+    def query_cache_enabled
+     @query_cache_enabled || @master.connection.query_cache_enabled
     end
   end
 end
--- spec/connection_proxy_spec.rb
+++ spec/connection_proxy_spec.rb
@@ -146,7 +146,7 @@ describe SlavePools do
         3.times { @proxy.select_all(@sql) }
       end
     end
-  
+
     it 'should invalidate the cache on insert, delete and update' do
       ActiveRecord::Base.cache do
         meths = [:insert, :update, :delete, :insert, :update]
@@ -162,7 +162,40 @@ describe SlavePools do
         end
       end
     end
-  
+
+    describe "using querycache middleware" do
+      it 'should cache queries using select_all' do
+        mw = ActiveRecord::QueryCache.new lambda { |env|
+          @default_slave1.should_receive(:select_all).exactly(1).and_return([])
+          @default_slave2.should_not_receive(:select_all)
+          @master.should_not_receive(:select_all)
+          3.times { @proxy.select_all(@sql) }
+          @proxy.next_slave!
+          3.times { @proxy.select_all(@sql) }
+          @proxy.next_slave!
+          3.times { @proxy.select_all(@sql)}
+        }
+        mw.call({})
+      end
+
+      it 'should invalidate the cache on insert, delete and update' do
+        mw = ActiveRecord::QueryCache.new lambda { |env|
+          meths = [:insert, :update, :delete, :insert, :update]
+          meths.each do |meth|
+            @master.should_receive(meth).and_return(true)
+          end
+          @default_slave1.should_receive(:select_all).exactly(5).and_return([])
+          @default_slave2.should_receive(:select_all).exactly(0)
+          5.times do |i|
+            @proxy.select_all(@sql)
+            @proxy.select_all(@sql)
+            @proxy.send(meths[i])
+          end
+        }
+        mw.call({})
+      end
+    end
+
     it 'should try a slave once and fall back to the master (should not retry other slaves)' do
       @default_slave1.should_receive(:select_all).once.and_raise(RuntimeError)
       @default_slave2.should_not_receive(:select_all)
