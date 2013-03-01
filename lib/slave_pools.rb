172164d18ed12e57b38c5e2f105ee0f305e97601
Aaron Suggs <aaron@ktheory.com>
2013-03-01 10:10:07 -0500

    whitespace

--- lib/slave_pools.rb
+++ lib/slave_pools.rb
@@ -8,19 +8,19 @@ require 'slave_pools/connection_proxy'
 #wrapper class to make the calls more succinct
 
 class SlavePools
-  
+
   def self.setup!
     SlavePoolsModule::ConnectionProxy.setup!
   end
-  
+
   def self.active?
     ActiveRecord::Base.respond_to?('connection_proxy')
   end
-  
+
   def self.next_slave!
     ActiveRecord::Base.connection_proxy.next_slave! if active?
   end
-  
+
   def self.with_pool(pool_name = 'default')
     if active?
       ActiveRecord::Base.connection_proxy.with_pool(pool_name) { yield }
@@ -28,7 +28,7 @@ class SlavePools
       yield
     end
   end
-  
+
   def self.with_master
     if active?
       ActiveRecord::Base.connection_proxy.with_master { yield }
