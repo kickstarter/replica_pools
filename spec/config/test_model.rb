2e84f9efd63233fc9795737b74a119302530fbc9
Tieg Zaharia <tieg.zaharia@gmail.com>
2014-01-25 06:14:19 -0500

    Remove the ex-Observer spec & TestSub class (unnecessary)

--- spec/config/test_model.rb
+++ spec/config/test_model.rb
@@ -1,13 +1,2 @@
 class TestModel < ActiveRecord::Base
-  has_many :test_subs
-
-  after_create :create_sub
-  def create_sub
-    ActiveRecord::Base.logger.info "in callback"
-    TestSub.create(:test_model=>self)
-  end
-end
-
-class TestSub < ActiveRecord::Base
-  belongs_to :test_model
 end
