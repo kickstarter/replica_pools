b872e9176ef073df2d3801849f93a3314492e616
Patrick Ellis <patrick.j.ellis@gmail.com>
2018-07-26 12:05:11 -0400

    Use docker-compose to run tests in development
    Also: fix TravisCI build

--- spec/config/bootstrap.sql
+++ spec/config/bootstrap.sql
@@ -1,3 +1,3 @@
 -- Create MySQL db & user for running specs
 create database IF NOT EXISTS test_db;
-grant select on test_db.* to 'read_only'@'localhost' identified by 'readme';
+grant select on test_db.* to 'read_only' identified by 'readme';
