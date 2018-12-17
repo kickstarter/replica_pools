96bb65b9d2d5051d3a14e4bba51a7c09ee0dd976
Tieg Zaharia <tieg.zaharia@gmail.com>
2018-12-17 17:37:49 -0500

    Add some more safe methods for Rails 5.0.

--- lib/replica_pools/engine.rb
+++ lib/replica_pools/engine.rb
@@ -26,7 +26,10 @@ module ReplicaPools
            :select_all, :select_one, :select_value, :select_values,
            :select_rows, :select, :select_prepared, :verify!, :raw_connection,
            :active?, :reconnect!, :disconnect!, :reset_runtime, :log,
-           :schema_cache, :lookup_cast_type_from_column, :clear_cache!, :sanitize_limit, :combine_bind_parameters
+           :lookup_cast_type_from_column, :sanitize_limit,
+           :combine_bind_parameters, :quote_table_name, :quote, :quote_column_names, :quote_table_names,
+           :case_sensitive_comparison, :case_insensitive_comparison,
+           :schema_cache, :cacheable_query, :prepared_statements, :clear_cache!
           ]
         else
           warn "Unsupported ActiveRecord version #{ActiveRecord.version}. Please whitelist the safe methods."
